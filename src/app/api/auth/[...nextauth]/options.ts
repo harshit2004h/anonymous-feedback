76501a2aeccc9b64ce0e11591ea49f9f7a36afb9
harshit2004h <hashitsrivastava2004h@gmail.com>
2025-03-24 08:40:37 +0530

    Implement user verification endpoint and update authentication options

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user.model";
 
-export const AuthOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       id: "credentials",
